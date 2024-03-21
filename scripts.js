785b447c430d528aa4020b39ddcccd6d8cd7686b
Prakhar <ptofficial29@gmail.com>
2024-03-21 23:22:27 +0530

    landing page done

---
+++ scripts.js
@@ -0,0 +1,65 @@
+function runAnimations() {
+  //   runAnimations_275();
+
+  document.querySelector("body").addEventListener("click", func_1199_11100);
+
+  function func_1199_11100(e) {
+    // click the canvas tag
+    document.getElementsByTagName("canvas")[0].click();
+
+    e.stopPropagation();
+
+    // document.querySelector("#id-254").classList.add("animationClass-254");
+    // document.querySelector("#id-1199").classList.add("animationClass-1199");
+    // document
+    //   .querySelector("#id-11112")
+    //   .classList.add("animationClass-null11112");
+    // document
+    //   .querySelector("#id-11112 > div:first-child")
+    //   .classList.add("bgClass-11112");
+    document
+      .querySelector(".hey-1-55401-0")
+      .classList.add("animationClass-11133-11133-0");
+    document.querySelector("#id-11133").classList.add("animationClass-11133");
+    document
+      .querySelector(".im-1-439989-0")
+      .classList.add("animationClass-11135-11135-0");
+    document.querySelector("#id-11135").classList.add("animationClass-11135");
+    document.querySelector("#id-11134").classList.add("animationClass-11134");
+    document.querySelector("#id-1183").classList.add("animationClass-1183");
+    document.querySelector("#id-1188").classList.add("animationClass-1188");
+    document
+      .querySelector(".prakhartandon-1-88939-0")
+      .classList.add("animationClass-1184-1184-0");
+    document.querySelector("#id-1184").classList.add("animationClass-1184");
+    document
+      .querySelector(".contactme-1-221553-0")
+      .classList.add("animationClass-1185-1185-0");
+    document.querySelector("#id-1185").classList.add("animationClass-1185");
+    document.querySelector("#id-1186").classList.add("animationClass-1186");
+    document
+      .querySelector("#id-1186 > div:first-child")
+      .classList.add("bgClass-11108");
+    document.querySelector("#id-1187").classList.add("animationClass-1187");
+    document
+      .querySelector("#id-1187 > div:first-child")
+      .classList.add("bgClass-11109");
+    document
+      .querySelector("#id-11101")
+      .classList.add("animationClass-null11101");
+    document
+      .querySelector("#id-11102")
+      .classList.add("animationClass-null11102");
+    document
+      .querySelector("#id-11103")
+      .classList.add("animationClass-null11103");
+    document
+      .querySelector("#id-11104")
+      .classList.add("animationClass-null11104");
+    document.querySelector("#id-275").classList.add("animationClass-275");
+    // document
+    //   .querySelector("#id-1199")
+    //   .removeEventListener("click", func_1199_11100);
+  }
+}
+runAnimations();
